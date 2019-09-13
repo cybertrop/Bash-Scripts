c828f77797f0ce75cba9ca9a383483bad486c0cb
John Doe <39686917+cybertrop@users.noreply.github.com>
2019-09-13 15:48:57 -0500

    Update file_carve.sh

--- file_carve.sh
+++ file_carve.sh
@@ -1,15 +1,29 @@
-# File carving examples
-
+# Log Analysis and File Carving examples
 #!/bin/bash
 
-cat md5.json | grep -Eo "md5.*" | awk -F "'" '{print $3,$7,$29}' | sed 's/|.....//g' | uniq | sort -u > exampl1.txt
-# Cat'ing a json file |  greping for a regex expression only matching the following string | using awk to output -
-# specific fields | sed to substitute/remove the following string "|....." from a hostname | uniq and sorting our list to deduplicate
+# Example 1:
+# Cat'ing a JSON file to grep for any MD5 hash-browns and then using awk to output -
+# specific fields then cleaning with sed to substitute/remove the following string "|....." from a hostname 
+# and uniq and sorting our list to deduplicate
+cat md5.json 
+| grep -Eo "md5.*" 
+| awk -F "'" '{print $3,$7,$29}' 
+| sed 's/|.....//g' 
+| uniq 
+| sort -u > exampl1.txt
 
-cat parse4.txt | sed "s/'//g" | tr -s ',[a-z]' ',[A-Z]' > parse5.txt
+# Example 2
 # Cat'ing out a text file | sed substitution/removal of an apostrophe | tr (translate) to subsittute a regex expression
 # > output file
+cat parse4.txt 
+| sed "s/'//g" 
+| tr -s ',[a-z]' ',[A-Z]' > parse5.txt
 
-cat parse9.csv | cut -d "," -f 2 | sort -u > parse9.txt ; cat parse9.txt | wc -l
+# Example 3
 # Cat'ing a csv | cuting a file to output the second occurrence of a coma delimter | sorting for uniqueness | outputting and -
 # determing length using wc -l 
+cat parse9.csv 
+| cut -d "," -f 2 
+| sort -u > parse9.txt ; cat parse9.txt 
+| wc -l
+
